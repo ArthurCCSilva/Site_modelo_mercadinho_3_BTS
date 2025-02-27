3c16cab8bf102640e1e84e623d926ac6ccbf1b19
ArthurCCSilva <arthurccs2013@gmail.com>
2025-02-26 22:16:02 -0300

    produtos
    atualização dos produtos e suas páginas

--- js/carne.js
+++ js/carne.js
@@ -1,13 +1,15 @@
-const allProducts = [
+const listaProdutos = [
     { 
-        name: "Salsicha - Hot Dog", 
+        id: 2,
+        name: "Salsicha - Hot Dog",  
         description: "Salsicha para cachorro-quente, prática e deliciosa.", 
         valor: "R$ 8,00", 
         price: 8.00, 
         category: "carne", 
         image: "img/imgproduto196.jpg"    
     },
-    { 
+    {   
+        id: 7,
         name: "Carne - Suína", 
         description: "Carne suína fresca para churrasco e pratos rápidos.", 
         valor: "R$ 20,00", 
@@ -16,6 +18,7 @@ const allProducts = [
         image: "img/imgproduto196.jpg"    
     },
     { 
+        id: 55,
         name: "Carne - Bovina", 
         description: "Carne bovina fresca para churrascos e grelhados.", 
         valor: "R$ 25,99", 
@@ -24,6 +27,7 @@ const allProducts = [
         image: "img/imgproduto196.jpg"    
     },
     { 
+        id: 56,
         name: "Frango - Resfriado", 
         description: "Frango resfriado fresco para assados e grelhados.", 
         valor: "R$ 12,99", 
@@ -32,6 +36,7 @@ const allProducts = [
         image: "img/imgproduto196.jpg"    
     },
     { 
+        id: 57,
         name: "Peixe - Tilápia", 
         description: "Peixe tilápia fresco para grelhados e assados.", 
         valor: "R$ 15,99", 
@@ -40,6 +45,7 @@ const allProducts = [
         image: "img/imgproduto196.jpg"    
     },
     { 
+        id: 58,
         name: "Linguiça - Calabresa", 
         description: "Linguiça calabresa defumada para churrascos e petiscos.", 
         valor: "R$ 15,00", 
@@ -48,6 +54,7 @@ const allProducts = [
         image: "img/imgproduto196.jpg"    
     },
     { 
+        id: 59,
         name: "Costela - Bovina", 
         description: "Costela bovina para assados lentos e saborosos.", 
         valor: "R$ 30,00", 
@@ -56,6 +63,7 @@ const allProducts = [
         image: "img/imgproduto196.jpg"    
     },
     { 
+        id: 60,
         name: "Peixe - Salmão", 
         description: "Peixe salmão fresco para grelhados e ceviches.", 
         valor: "R$ 50,00", 
@@ -64,6 +72,7 @@ const allProducts = [
         image: "img/imgproduto196.jpg"    
     },
     { 
+        id: 72,
         name: "Linguiça - Toscana", 
         description: "Linguiça toscana temperada para grelhados.", 
         valor: "R$ 18,00", 
@@ -72,7 +81,8 @@ const allProducts = [
         image: "img/imgproduto196.jpg"    
     },
     { 
-        name: "Frango - Desossado", 
+        id: 73,
+        name: "Frango - Desossado",  
         description: "Peito de frango desossado para preparações rápidas.", 
         valor: "R$ 14,99", 
         price: 14.99, 
@@ -80,6 +90,7 @@ const allProducts = [
         image: "img/imgproduto196.jpg"    
     },
     { 
+        id: 74,
         name: "Paleta - Suína", 
         description: "Paleta suína ideal para assados lentos.", 
         valor: "R$ 22,00", 
@@ -88,6 +99,7 @@ const allProducts = [
         image: "img/imgproduto196.jpg"    
     },
     { 
+        id: 75,
         name: "Filé Mignon - Bovino", 
         description: "Filé mignon bovino para cortes refinados e churrascos.", 
         valor: "R$ 45,00", 
